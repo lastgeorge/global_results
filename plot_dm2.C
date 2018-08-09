181727ea79a79ae085b4b4a35261938900cdb52c
Xin Qian <last.qx@gmail.com>
2018-08-09 11:05:12 -0400

    update

--- plot_dm2.C
+++ plot_dm2.C
@@ -1,34 +1,60 @@
 void plot_dm2(){
   TCanvas *c1 = new TCanvas("c1","c1",800,1000);
   c1->SetFillColor(10);
-  double idx[7]={1,7,3,2,4,5,6};
-  double value[7]={2.45,2.57,2.42,2.545,2.67,2.50,2.50};
-  double valueUncLow[7]={0.08,0.264,0.09,0.082,0.12,0.2,0.2}; 
-  double valueUncHigh[7]={0.08,0.242,0.09,0.084,0.12,0.2,0.19};
+  // Neutrino 2016 
+  // double idx[7]={1,7,3,2,4,5,6};
+  // double value[7]={2.45,2.57,2.42,2.545,2.67,2.50,2.50};
+  // double valueUncLow[7]={0.08,0.264,0.09,0.082,0.12,0.2,0.2}; 
+  // double valueUncHigh[7]={0.08,0.242,0.09,0.084,0.12,0.2,0.19};
+
+  //Neutrino 2018
+  double idx[8]={2,7,8,1,3,5,4,6};
+  double value[8]={2.47,2.63,2.48,2.434,2.42,2.51,2.50,2.31};
+  double valueUncLow[8]={0.07,0.14,0.28,0.064,0.085,0.08,0.12,0.13}; 
+  double valueUncHigh[8]={0.07,0.14,0.32,0.064,0.085,0.12,0.05,0.11};
+
+ 
+
   
-  TGraphAsymmErrors *oscGraph = new TGraphAsymmErrors(2, value, idx, valueUncLow, valueUncHigh, 0, 0);
+  
+  TGraphAsymmErrors *oscGraph = new TGraphAsymmErrors(3, value, idx, valueUncLow, valueUncHigh, 0, 0);
   oscGraph->SetTitle("");
   oscGraph->SetLineColor(2);
   oscGraph->SetLineWidth(1);
   oscGraph->SetMarkerColor(2);
   oscGraph->SetMarkerSize(1.5);
   oscGraph->SetMarkerStyle(20);
-  oscGraph->GetXaxis()->SetLimits(2.2,3.2);
-  oscGraph->GetYaxis()->SetRangeUser(0,8.);
+  oscGraph->GetXaxis()->SetLimits(2.1,3.3);
+  oscGraph->GetYaxis()->SetRangeUser(0,9.);
   oscGraph->GetYaxis()->SetNdivisions(0);
   oscGraph->GetXaxis()->SetTitle("#Delta m^{2}_{32} (#times 10^{-3} eV^{2})");
   oscGraph->SetTitle("Normal Hierarchy Assumed");
   oscGraph->Draw("AP");
+
+
+
   
   TLatex latex;
   latex.SetTextSize(0.05);
   latex.SetTextColor(2);
-  latex.DrawLatex(2.9,idx[0]-0.1,"Daya Bay");
+  latex.DrawLatex(2.83,idx[0]-0.1,"Daya Bay");
   latex.SetTextColor(2);
-  latex.DrawLatex(2.9,idx[1]-0.1,"RENO");
+  latex.DrawLatex(2.83,idx[1]-0.1,"RENO (nGd)");
+  latex.DrawLatex(2.83,idx[2]-0.1,"RENO (nH)");
 
+
+  Double_t dyb_update = value[3];
+  Double_t dyb_update_low = valueUncLow[3];
+  Double_t dyb_update_high = valueUncHigh[3];
+  Double_t dyb_update_x = -4.0;
+  TBox box;
+  box.SetFillColor(17);
+  box.DrawBox(dyb_update-dyb_update_low, 9.0, 
+	      dyb_update+dyb_update_high, 0.0);
+  oscGraph->Draw("Psame");
   
-  TGraphAsymmErrors *oscGraph1 = new TGraphAsymmErrors(3, &value[2], &idx[2], &valueUncLow[2], &valueUncHigh[2], 0, 0);
+  
+  TGraphAsymmErrors *oscGraph1 = new TGraphAsymmErrors(3, &value[3], &idx[3], &valueUncLow[3], &valueUncHigh[3], 0, 0);
   oscGraph1->SetTitle("");
   oscGraph1->SetLineColor(4);
   oscGraph1->SetLineWidth(1);
@@ -43,13 +69,13 @@ void plot_dm2(){
   oscGraph1->Draw("Psame");
 
   latex.SetTextColor(4);
-  latex.DrawLatex(2.9,idx[2]-0.1,"MINOS");
+  latex.DrawLatex(2.83,idx[4]-0.1,"MINOS(+)");
   latex.SetTextColor(4);
-  latex.DrawLatex(2.9,idx[3]-0.1,"T2K");
+  latex.DrawLatex(2.83,idx[3]-0.1,"T2K");
   latex.SetTextColor(4);
-  latex.DrawLatex(2.9,idx[4]-0.1,"NO#nuA");
+  latex.DrawLatex(2.83,idx[5]-0.1,"NO#nuA");
 
-  TGraphAsymmErrors *oscGraph2 = new TGraphAsymmErrors(2, &value[5], &idx[5], &valueUncLow[5], &valueUncHigh[5], 0, 0);
+  TGraphAsymmErrors *oscGraph2 = new TGraphAsymmErrors(2, &value[6], &idx[6], &valueUncLow[6], &valueUncHigh[6], 0, 0);
   oscGraph2->SetTitle("");
   oscGraph2->SetLineColor(8);
   oscGraph2->SetLineWidth(1);
@@ -64,8 +90,8 @@ void plot_dm2(){
   oscGraph2->Draw("Psame");
 
   latex.SetTextColor(8);
-  latex.DrawLatex(2.9,idx[5]-0.1,"Super-K");
+  latex.DrawLatex(2.83,idx[6]-0.1,"Super-K");
   latex.SetTextColor(8);
-  latex.DrawLatex(2.9,idx[6]-0.1,"IceCube");
+  latex.DrawLatex(2.83,idx[7]-0.1,"IceCube");
   
 }
--- plot_th13.C
+++ plot_th13.C
@@ -1,14 +1,24 @@
 void plot_th13(){
   TCanvas *c1 = new TCanvas("c1","c1",800,1000);
   c1->SetFillColor(10);
-  double idx[4]={1,2,3,4,};
-  double value[4]={0.0841,0.071,0.082,0.119,
-  };
-  double valueUncLow[4]={0.0033,0.011,0.0108,0.016,
-  };
-  double valueUncHigh[4]={0.0033,0.011,0.0108,0.016,
-  };
+  // Neutrino 2016
+  // double idx[4]={1,2,3,4,};
+  // double value[4]={0.0841,0.071,0.082,0.119,
+  // };
+  // double valueUncLow[4]={0.0033,0.011,0.0108,0.016,
+  // };
+  // double valueUncHigh[4]={0.0033,0.011,0.0108,0.016,
+  // };
 
+  //  Neutrino 2018
+  double idx[5]={1,3,2,4,5};
+  double value[5]={0.0856,0.071,0.0896,0.085,0.105,
+  };
+  double valueUncLow[5]={0.0029,0.011,0.0068,0.014,0.014
+  };
+  double valueUncHigh[5]={0.0029,0.011,0.0068,0.014,0.014
+  };
+  
   Double_t dyb_update = value[0];
   Double_t dyb_update_low = valueUncLow[0];
   Double_t dyb_update_high = valueUncHigh[0];
@@ -22,14 +32,14 @@ void plot_th13(){
   oscGraph->SetMarkerSize(1.5);
   oscGraph->SetMarkerStyle(20);
   oscGraph->GetXaxis()->SetLimits(0,0.25);
-  oscGraph->GetYaxis()->SetRangeUser(0,5.);
+  oscGraph->GetYaxis()->SetRangeUser(0,6.);
   oscGraph->GetYaxis()->SetNdivisions(0);
   oscGraph->GetXaxis()->SetTitle("sin^{2}2#theta_{13}");
   oscGraph->Draw("AP");
   
   TBox box;
   box.SetFillColor(17);
-  box.DrawBox(dyb_update-dyb_update_low, 5.0, 
+  box.DrawBox(dyb_update-dyb_update_low, 6.0, 
 	      dyb_update+dyb_update_high, 0.0);
 
   oscGraph->Draw("Psame");
@@ -43,7 +53,7 @@ void plot_th13(){
   latex.DrawLatex(0.14,idx[1]-0.1,"Daya Bay (nH)");
 
 
-  TGraphAsymmErrors *oscGraph1 = new TGraphAsymmErrors(1, &value[2], &idx[2], &valueUncLow[2], &valueUncHigh[2], 0, 0);
+  TGraphAsymmErrors *oscGraph1 = new TGraphAsymmErrors(2, &value[2], &idx[2], &valueUncLow[2], &valueUncHigh[2], 0, 0);
   oscGraph1->SetTitle("");
   oscGraph1->SetLineColor(8);
   oscGraph1->SetLineWidth(1);
@@ -57,9 +67,10 @@ void plot_th13(){
   oscGraph1->Draw("Psame");
 
   latex.SetTextColor(8);
-  latex.DrawLatex(0.14,idx[2]-0.1,"RENO");
+  latex.DrawLatex(0.14,idx[2]-0.1,"RENO (nGd)");
+  latex.DrawLatex(0.14,idx[3]-0.1,"RENO (nH)");
 
-  TGraphAsymmErrors *oscGraph2 = new TGraphAsymmErrors(1, &value[3], &idx[3], &valueUncLow[3], &valueUncHigh[3], 0, 0);
+  TGraphAsymmErrors *oscGraph2 = new TGraphAsymmErrors(1, &value[4], &idx[4], &valueUncLow[4], &valueUncHigh[3], 0, 0);
   oscGraph2->SetTitle("");
   oscGraph2->SetLineColor(4);
   oscGraph2->SetLineWidth(1);
@@ -73,8 +84,8 @@ void plot_th13(){
   oscGraph2->Draw("Psame");
 
   latex.SetTextColor(4);
-  latex.DrawLatex(0.14,idx[3]-0.1,"Double Chooz");
-  
+  latex.DrawLatex(0.14,idx[4]-0.1,"Double Chooz ");
+  latex.DrawLatex(0.14,idx[4]-0.5,"(nGd+nH)");
   
 
  
